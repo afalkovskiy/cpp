44e7e2e1873773aff4f5d0da9c81a3672fd09dfc
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-01-30 16:51:00 -0700

    Create rect2.cpp

---
+++ rect2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+
+    int rows, columns;
+    char symbol;
+
+    cout << "How many rows and columns do you want, and with what symbol (default     is *) ?" << endl;
+    cin >> rows >> columns >> symbol;
+    cout << endl;
+    
+    for (int y = 1; y <= rows; y++) {
+        for (int x = 1; x <= columns; x++) {
+            // if(x==y or x==int(columns/2))
+            if(x==y or x==floor(columns/2))
+                cout << "  " << symbol;
+            else 
+                cout << "   ";
+        }
+        cout << endl;
+    }
+
+    return(0);
+
+}
