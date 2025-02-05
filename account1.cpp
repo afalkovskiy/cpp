02197049d5994079459effadc10f907e2d9d774c
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-02-05 15:19:24 -0700

    Create account1.cpp

---
+++ account1.cpp
@@ -0,0 +1,13 @@
+// an example to start from
+#include <iostream>
+using namespace std;
+
+int main() {    
+    int number;
+
+    cout << "Enter an integer: ";
+    cin >> number;
+
+    cout << "You entered " << number;    
+    return 0;
+}
