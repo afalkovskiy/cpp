bf091c0a94422c4f3ef12a78453f0cbe3e3aa1ef
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-03-06 17:02:59 -0700

    Create FizzBuzz1.cpp

---
+++ FizzBuzz1.cpp
@@ -0,0 +1,10 @@
+// Runs with error - you will need to fix it!
+#include <iostream>
+using namespace std;
+int main ()
+{
+       cout << "Enter your number ";      // Prompt for input
+       cin >> n;                          // Get the input.
+
+       return 0;
+}
