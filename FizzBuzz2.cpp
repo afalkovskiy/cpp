32e74c116458fc3bdd10260b80dfedd440d5c92e
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-03-06 17:21:58 -0700

    Create FizzBuzz2.cpp

---
+++ FizzBuzz2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+       int i(0);
+       
+       cout << "Enter your number ";      // Prompt for input
+       cin >> i;                          // Get the input.
+
+        if ((i % 3) == 0)
+                       cout << "Fizz\n";  
+ // add here a piece of code to output Buzz if divisible by 5
+ 
+ 
+ 
+ // output FizzBuzz if divisible by 3 and 5
+
+       return 0;
+}
