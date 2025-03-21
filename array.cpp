6cf9db9362af2409fe4172034b35845282980d46
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-03-20 21:19:10 -0400

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <cstdlib>
 #include <string>
+#include <ctime> 
 using namespace std;
 
 int main() {
   string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
   cout << cars[0];
+  srand(time(0));
+  int randomNum = rand() % 3;
+  
+    cout << randomNum;
+cout << "\n" << cars[randomNum];
+  
+  
   return 0;
 }
