bfe324f04e71e615598bc2a6e56969dd5d3938a2
Alex Falkovskiy <alexander.falkovskiy@gmail.com>
2025-04-09 23:39:15 -0600

    Create array2.cpp

---
+++ array2.cpp
@@ -0,0 +1,13 @@
+// Online C++ compiler to run C++ program online
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{
+string cars[5] = {"Volvo", "BMW", "Ford", "Mazda", "Tesla"};
+
+// Loop through strings
+for (string car : cars) {
+  cout << car << "\n";
+}
+} 
